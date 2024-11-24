6cc724f8020cc46920f56d0b411d8bd08de753b6
Ubka12 <ubahkahie@outlook.com>
2024-11-24 13:17:17 +0000

    Added function

---
+++ MathFunction.js
@@ -0,0 +1,8 @@
+/********************************
+Developer: Full Name
+University ID: ID Here
+Function: This function takes two inputs as integers and returns their sum.
+********************************/
+function Add(x, y) {
+    return x + y;
+}
